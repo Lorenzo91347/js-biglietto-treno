f3dad4d1715275906012e112c4083a615b1b31cf
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-06 17:42:42 +0100

    feat alert to actual price

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 'use strict'
-
+alert('Welcome to the Cost Calculator for train tickets!');
 const km = prompt ('Insert the distance you want to travel in Km');
 const age = prompt ('Insert your age');
 
-let nKm =parseInt(km)
+let nKm =parseInt(km);
+let nAge =parseInt(age);
 
- let basePrice = (nKm *= 0.21);
+ let basePrice = (nKm *= 0.21); // default
 
- if (age < 18){ let revPrice = (basePrice / 0.8)}
+ if (nAge < 18){
+    basePrice = (basePrice - ((basePrice*20)/100))
+} else if(nAge > 65){
+    basePrice = (basePrice - ((basePrice*40)/100))
+} 
+// else {
+//     console.log(basePrice)
+// };
 
- console.log(revPrice)
+ alert(basePrice.toFixed(2));
