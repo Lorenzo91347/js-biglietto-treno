f62d6d1adc62783f60616d7363e46e8639e99b52
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-06 16:51:47 +0100

    feat:if

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 'use strict'
 
-const km = prompt ('Insert the distance you want to travel')
-const age = prompt ('Insert your age')
+const km = prompt ('Insert the distance you want to travel in Km');
+const age = prompt ('Insert your age');
+
+let nKm =parseInt(km)
+
+ let basePrice = (nKm *= 0.21);
+
+ if (age < 18){ let revPrice = (basePrice / 0.8)}
+
+ console.log(revPrice)
